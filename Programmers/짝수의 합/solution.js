b19dfd0b8c7d8444709cdf73f70889956c7a0d37
L2HYUNN <donghyuni98@gmail.com>
2023-01-13 02:03:32 +0900

    feat(even): add even sum solution

---
+++ Programmers/짝수의 합/solution.js
@@ -0,0 +1,5 @@
+function solution(n) {
+  // 수학을 잘하면 알고리즘 문제 해결이 쉽다.
+  var half = Math.floor(n / 2);
+  return half * (half + 1);
+}
---
+++ Programmers/짝수의 합/solution2.js
@@ -0,0 +1,9 @@
+function solution(n) {
+  return Array(n)
+    .fill()
+    .map((_, i) => i + 1)
+    .filter((v) => v % 2 === 0)
+    .reduce((prev, curr) => prev + curr);
+}
+
+console.log(solution(10));
