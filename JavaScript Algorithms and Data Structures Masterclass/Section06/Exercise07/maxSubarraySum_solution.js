e9c68a89c21dd2bb4bbf5bb67b1de693fc1676b4
L2HYUNN <donghyuni98@gmail.com>
2023-02-17 00:54:36 +0900

    feat: maxSubarraySum 솔루션 및 체크사항 추가

---
+++ JavaScript Algorithms and Data Structures Masterclass/Section06/Exercise07/maxSubarraySum_solution.js
@@ -0,0 +1,14 @@
+function maxSubarraySum(arr, num) {
+  if (arr.length < num) return null;
+
+  let total = 0;
+  for (let i = 0; i < num; i++) {
+    total += arr[i];
+  }
+  let currentTotal = total;
+  for (let i = num; i < arr.length; i++) {
+    currentTotal += arr[i] - arr[i - num];
+    total = Math.max(total, currentTotal); // CHECK:: 수의 max를 구할 때 조건문에 비해 더욱 직관적이다.
+  }
+  return total;
+}
