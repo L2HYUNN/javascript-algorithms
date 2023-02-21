0c73ca66c400dda3de336b9bf198ebfb0448e3c2
L2HYUNN <donghyuni98@gmail.com>
2023-02-22 00:56:41 +0900

    feat: 백준 Node 풀이 템플릿 추가

---
+++ Baekjoon/index.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const path = require("path");
+const dir = path.join(__dirname, "/test.txt");
+// const dir = '/dev/stdin';
+
+const inputData = fs.readFileSync(dir).toString().trim().split("\n");
