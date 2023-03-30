27e87d483c243a010c7df84eecf8a7a21afe7bb8
L2HYUNN <donghyuni98@gmail.com>
2023-03-30 20:52:24 +0900

    feat: string search 솔루션 및 풀이 추가

--- JavaScript Algorithms and Data Structures Masterclass/Section10/lecture64/string_search.js
+++ JavaScript Algorithms and Data Structures Masterclass/Section10/lecture64/string_search.js
@@ -1,12 +1,25 @@
-function naiveSearch(long, short){
-    var count = 0;
-    for(var i = 0; i < long.length; i++){
-        for(var j = 0; j < short.length; j++){
-           if(short[j] !== long[i+j]) break;
-           if(j === short.length - 1) count++;
-        }
-    }
-    return count;
+/**
+ * @description
+ * 긴 문자열에 있는 문자 하나와 짧은 문자열에 있는 문자 하나를 서로 비교해가며 match 여부를 확인한다.
+ *
+ * @param {string} long
+ * @param {string} short
+ *
+ * @returns {string} matchedStringCounter
+ */
+function stringSearch(long, short) {
+  let matchedStringCounter = 0;
+  let shortPointer = 0;
+
+  for (let i = 0; i < long.length; i++) {
+    long[i] === short[shortPointer] ? shortPointer++ : (shortPointer = 0);
+
+    if (shortPointer === short.length - 1) matchedStringCounter++;
+  }
+
+  return matchedStringCounter;
 }
 
-naiveSearch("lorie loled", "lol")
+console.log(stringSearch("stringringgrin", "rin"));
+console.log(stringSearch("lorie loled", "lol"));
+console.log(stringSearch("lorie loled", "abc"));
