66529b15f99a918afc05083111e8a93abeb077f1
L2HYUNN <donghyuni98@gmail.com>
2023-05-10 18:44:58 +0900

    refactor: radix helper function이 음수에도 대응할 수 있게 변경

--- JavaScript Algorithms and Data Structures Masterclass/Section17/lecture100/radix_helpers.js
+++ JavaScript Algorithms and Data Structures Masterclass/Section17/lecture100/radix_helpers.js
@@ -1,5 +1,5 @@
 function getDigit(num, place) {
-  const stringifiedNum = num.toString();
+  const stringifiedNum = Math.abs(num).toString();
   return Number(stringifiedNum[stringifiedNum.length - (place + 1)]);
 }
 
@@ -9,11 +9,13 @@ console.log(getDigit(123456, 2));
 console.log(getDigit(123456, 3));
 console.log(getDigit(123456, 4));
 console.log(getDigit(123456, 5));
+console.log(getDigit(-123456, 5));
 
 function digitCount(num) {
-  return num.toString().length;
+  return Math.abs(num).toString().length;
 }
 
 console.log(digitCount(1));
 console.log(digitCount(25));
 console.log(digitCount(314));
+console.log(digitCount(-314));
