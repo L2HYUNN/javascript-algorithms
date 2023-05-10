598e6dbb224daeea2813414d13289636525368f2
L2HYUNN <donghyuni98@gmail.com>
2023-05-10 18:42:22 +0900

    feat: digitCount 풀이 추가

--- JavaScript Algorithms and Data Structures Masterclass/Section17/lecture100/radix_helpers.js
+++ JavaScript Algorithms and Data Structures Masterclass/Section17/lecture100/radix_helpers.js
@@ -9,3 +9,11 @@ console.log(getDigit(123456, 2));
 console.log(getDigit(123456, 3));
 console.log(getDigit(123456, 4));
 console.log(getDigit(123456, 5));
+
+function digitCount(num) {
+  return num.toString().length;
+}
+
+console.log(digitCount(1));
+console.log(digitCount(25));
+console.log(digitCount(314));
