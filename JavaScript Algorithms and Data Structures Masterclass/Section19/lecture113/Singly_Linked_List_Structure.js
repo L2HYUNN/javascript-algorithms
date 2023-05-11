4090d7acf2b996e9eba1e9994c2235c009d5e895
L2HYUNN <donghyuni98@gmail.com>
2023-05-12 00:07:02 +0900

    feat: 단일 연결 리스트 pop 메소드 구현

---
+++ JavaScript Algorithms and Data Structures Masterclass/Section19/lecture113/Singly_Linked_List_Structure.js
@@ -0,0 +1,62 @@
+// piece of data - val
+//reference to next node - next
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+  pop() {
+    let current = this.head;
+    let prev = null;
+
+    while (current.next) {
+      prev = current;
+      current = current.next;
+    }
+
+    prev.next = null;
+    this.tail = prev;
+
+    return current.val;
+  }
+}
+
+// var: 1, next: {} -> prev
+// var: 2, next: null -> current
+
+// var first = new Node("Hi")
+// first.next = new Node("there")
+// first.next.next = new Node("how")
+// first.next.next.next = new Node("are")
+// first.next.next.next.next = new Node("you")
+
+var list = new SinglyLinkedList();
+list.push("HELLO");
+list.push("GOODBYE");
+list.push("RE HELLO");
+
+console.log(list);
+console.log(list.pop());
+console.log(list);
