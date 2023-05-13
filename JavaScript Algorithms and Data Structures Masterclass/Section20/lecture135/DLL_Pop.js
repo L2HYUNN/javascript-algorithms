395b1d943b57f41826d046ae9f93bb4ef69ce3fd
L2HYUNN <donghyuni98@gmail.com>
2023-05-13 20:08:01 +0900

    feat: 양방향 연결 리스트 pop 메소드 구현

---
+++ JavaScript Algorithms and Data Structures Masterclass/Section20/lecture135/DLL_Pop.js
@@ -0,0 +1,48 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    const targetNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      targetNode.prev = null;
+    }
+    this.length--;
+    return targetNode;
+  }
+}
+
+const list = new DoublyLinkedList();
+list.push("first");
+console.log(list);
+list.pop();
+console.log(list);
