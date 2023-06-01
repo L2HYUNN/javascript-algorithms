87ee1b67b2023f52bed4952ffdf45f6cdccee747
L2HYUNN <donghyuni98@gmail.com>
2023-06-01 22:00:30 +0900

    feat: 알파벳 개수 풀이 및 참고 자료 추가

---
+++ Baekjoon/알파벳 개수/solution.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const path = require("path");
+const dir = path.join(__dirname, "/test.txt");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : dir;
+const input = fs.readFileSync(filePath).toString().trim().split("\n")[0];
+
+function solution(input) {
+  const result = {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0,
+    g: 0,
+    h: 0,
+    i: 0,
+    j: 0,
+    k: 0,
+    l: 0,
+    m: 0,
+    n: 0,
+    o: 0,
+    p: 0,
+    q: 0,
+    r: 0,
+    s: 0,
+    t: 0,
+    u: 0,
+    v: 0,
+    w: 0,
+    x: 0,
+    y: 0,
+    z: 0,
+  };
+
+  input.split("").forEach((i) => {
+    result[i] += 1;
+  });
+
+  return Object.values(result).join(" ");
+}
+
+console.log(solution(input));
