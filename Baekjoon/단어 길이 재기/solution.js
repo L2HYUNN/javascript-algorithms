1a5983e8f125c6c8e5258cfff0b93995abbeacb7
L2HYUNN <donghyuni98@gmail.com>
2023-06-03 23:24:51 +0900

    feat: 단어 길이 재기 풀이 추가

---
+++ Baekjoon/단어 길이 재기/solution.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+const dir = path.join(__dirname, "/test.txt");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : dir;
+const input = fs.readFileSync(filePath).toString().trim().split("\n")[0];
+
+function solution(input) {
+  return input.length;
+}
+
+console.log(solution(input));
