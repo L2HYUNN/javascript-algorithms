f27e65a36abdf49953148219065d73f0c50a0d36
L2HYUNN <donghyuni98@gmail.com>
2023-06-04 02:59:23 +0900

    feat: 네 수 풀이 추가

---
+++ Baekjoon/네 수/solution.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const path = require("path");
+const dir = path.join(__dirname, "/test.txt");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : dir;
+const input = fs.readFileSync(filePath).toString().trim().split("\n")[0];
+
+function solution(input) {
+  const [a, b, c, d] = input.split(" ");
+  return +(a + b) + +(c + d);
+}
+
+console.log(solution(input));
