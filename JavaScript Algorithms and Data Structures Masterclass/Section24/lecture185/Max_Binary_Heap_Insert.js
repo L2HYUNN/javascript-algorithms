d96ac780550ed76c9748800bb9c848b7c8882d25
L2HYUNN <donghyuni98@gmail.com>
2023-06-08 15:25:42 +0900

    refactor: max binary heap insert 메소드 bubbleup 메소드 추출

--- JavaScript Algorithms and Data Structures Masterclass/Section24/lecture185/Max_Binary_Heap_Insert.js
+++ JavaScript Algorithms and Data Structures Masterclass/Section24/lecture185/Max_Binary_Heap_Insert.js
@@ -2,23 +2,24 @@ class MaxBinaryHeap {
   constructor() {
     this.values = [];
   }
-  bubbleUp() {}
-  insert(value) {
-    this.values.push(value);
+  bubbleUp() {
     let currentIndex = this.values.length - 1;
+    const value = this.values[currentIndex];
 
     while (currentIndex > 0) {
       let parentIndex = Math.floor(currentIndex - 1 / 2);
       let parent = this.values[parentIndex];
 
-      if (parent < value) {
-        this.values[parentIndex] = value;
-        this.values[currentIndex] = parent;
-        currentIndex = parentIndex;
-      } else {
-        break;
-      }
+      if (parent >= value) break;
+      this.values[parentIndex] = value;
+      this.values[currentIndex] = parent;
+      currentIndex = parentIndex;
     }
+  }
+
+  insert(value) {
+    this.values.push(value);
+    this.bubbleUp();
   }
 }
 
