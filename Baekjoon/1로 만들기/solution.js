6b936720f8b85ef38fa10b88c1ec0a73a3d29131
L2HYUNN <donghyuni98@gmail.com>
2024-04-09 13:25:16 +0900

    feat: 1로 만들기 풀이 추가

---
+++ Baekjoon/1로 만들기/solution.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+const dir = path.join(__dirname, "/test.txt");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : dir;
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+function solution(input) {
+  const n = parseInt(input[0]);
+
+  const DP = new Array(n + 1).fill(0);
+
+  for (let i = 2; i <= n; i++) {
+    DP[i] = DP[i - 1] + 1;
+
+    if (i % 2 === 0) {
+      DP[i] = Math.min(DP[i], DP[i / 2] + 1);
+    }
+
+    if (i % 3 === 0) {
+      DP[i] = Math.min(DP[i], DP[i / 3] + 1);
+    }
+  }
+
+  return DP[n];
+}
+
+console.log(solution(input));
