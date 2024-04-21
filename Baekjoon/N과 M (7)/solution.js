883f24b80aefaa5571dd477b72bb9c85cfbab8be
L2HYUNN <donghyuni98@gmail.com>
2024-04-21 16:16:38 +0900

    feat: n과 m (7) 풀이 추가

---
+++ Baekjoon/N과 M (7)/solution.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const path = require("path");
+const dir = path.join(__dirname, "/test.txt");
+
+const filePath = process.platform === "linux" ? "/dev/stdin" : dir;
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+function solution(input) {
+  const [N, M] = input[0].split(" ").map(Number);
+  const numbers = input[1].split(" ").map(Number);
+
+  numbers.sort((a, b) => a - b);
+
+  return duplicatedPermutation(numbers, M);
+
+  function duplicatedPermutation(numbers, k) {
+    const result = [];
+
+    backtrack([]);
+
+    return result;
+
+    function backtrack(path) {
+      if (path.length === k) {
+        result.push([...path]);
+        return;
+      }
+
+      for (let i = 0; i < numbers.length; i++) {
+        path.push(numbers[i]);
+        backtrack(path);
+        path.pop();
+      }
+    }
+  }
+}
+
+console.log(solution(input));
