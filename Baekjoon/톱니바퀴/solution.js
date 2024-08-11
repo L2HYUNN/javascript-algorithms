4c2823872897625476160665bb109a3f240546ad
L2HYUNN <donghyuni98@gmail.com>
2024-08-11 19:25:23 +0900

    feat: 톱니바퀴 풀이 추가

--- Baekjoon/톱니바퀴/solution.js
+++ Baekjoon/톱니바퀴/solution.js
@@ -6,75 +6,105 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : dir;
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 function solution(input) {
-  const gears = input.slice(0, 4).map((gear) => gear.split("").map(Number));
-  const K = Number(input[4]);
-  const rotateMethod = input
-    .slice(5, 5 + K)
-    .map((method) => method.split(" ").map(Number));
+  const GEARS = input
+    .slice(0, 4)
+    .map((gear) => gear.split("").map((str) => +str));
 
-  console.log(gears[2]);
+  const totalRotate = input[4];
+  const instructions = input
+    .slice(5)
+    .map((instruction) => instruction.split(" ").map((str) => +str));
 
-  console.log(rotateGear(gears[2], -1));
+  instructions.forEach(([gear, direction]) => {
+    const rotating = [];
 
-  rotateMethod.forEach((method) => {
-    return;
+    const visited = [false, false, false, false];
+
+    findRotatingGear([gear - 1, direction], GEARS, rotating, visited);
+
+    rotating.forEach(([gear, direction]) => {
+      const currentGear = GEARS[gear];
+      const newGear = rotate(currentGear, direction);
+      GEARS[gear] = newGear;
+    });
   });
-}
 
-// 12시 방향부터 시계방향으로 기어 정보가 주어진다.
-// 1번 기어의 묶여있는 번호는 2(우)번이다.
-// 2번 기어의 묶여있는 번호는 2(우), 6(좌)이다.
-// 3번 기어의 묶여있는 번호는 2(우), 6(좌)이다.
-// 4번 기어의 묶여있는 번호는 6(좌) 이다.
-
-function process(gears, method) {
-  const [number, direction] = method;
-
-  const coupledGear = isCoupledGear(gears);
-
-  const extraRotate = [];
-
-  if (number === 1) {
-    if (coupledGear[0]) {
-      extraRotate.push(2);
+  const result = GEARS.reduce((acc, cur, idx) => {
+    if (cur[0] === 1) {
+      if (idx === 0) {
+        return acc + 1;
+      } else if (idx === 1) {
+        return acc + 2;
+      } else if (idx === 2) {
+        return acc + 4;
+      } else {
+        return acc + 8;
+      }
+    } else {
+      return acc;
     }
+  }, 0);
+
+  function rotate(gear, direction) {
+    let newGear = [];
+
+    if (direction === 1) {
+      const last = gear.pop();
+      newGear = [last, ...gear];
+    }
+
+    if (direction === -1) {
+      const first = gear.shift();
+      newGear = [...gear, first];
+    }
+
+    return newGear;
   }
 
-  if (number === 3) {
-    if (coupledGear[1]) {
-      extraRotate.push(2);
-      if (coupledGear[0]) {
-        extraRotate.push(1);
+  function findRotatingGear(instruction, gears, rotating, visited) {
+    const [gear, direction] = instruction;
+
+    if (visited[gear]) {
+      return;
+    }
+
+    rotating.push(instruction);
+
+    visited[gear] = true;
+
+    const currentGear = gears[gear];
+    const [currentGearLeftTouched, currentGearRightTouched] = [
+      currentGear[6],
+      currentGear[2],
+    ];
+
+    if (gear - 1 >= 0) {
+      const prevGear = gears[gear - 1];
+      const [prevGearLeftTouched, prevGearRightTouched] = [
+        prevGear[6],
+        prevGear[2],
+      ];
+
+      if (prevGearRightTouched !== currentGearLeftTouched) {
+        const prevInstruction = [gear - 1, -direction];
+        findRotatingGear(prevInstruction, gears, rotating, visited);
       }
     }
 
-    if (coupledGear[2]) {
-      extraRotate.push(4);
+    if (gear + 1 < 4) {
+      const nextGear = gears[gear + 1];
+      const [nextGearLeftTouched, nextGearRightTouched] = [
+        nextGear[6],
+        nextGear[2],
+      ];
+
+      if (currentGearRightTouched !== nextGearLeftTouched) {
+        const nextInstruction = [gear + 1, -direction];
+        findRotatingGear(nextInstruction, gears, rotating, visited);
+      }
     }
 
-    rotateGear(gears[number - 1], direction);
-  }
-}
-
-function rotateGear(gear, direction) {
-  if (direction === 1) {
-    return [gear[gear.length], ...gear.slice(0, gear.length - 1)];
-  }
-
-  if (direction === -1) {
-    return [...gear.slice(1), gear[0]];
-  }
-}
-
-function isCoupledGear(gears) {
-  const result = [];
-
-  for (let i = 0; i < gears.length - 1; i++) {
-    if (gears[i][2] !== gears[i + 1][6]) {
-      result.push(true);
-    } else {
-      result.push(false);
-    }
+    return;
   }
 
   return result;
