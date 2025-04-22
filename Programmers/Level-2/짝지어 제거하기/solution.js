63f660e021488b39a151b7aadff9bcdcc50dde7c
L2HYUNN <donghyuni98@gmail.com>
2025-04-22 18:13:18 +0900

    feat: 짝지어 제거하기 풀이 추가

---
+++ Programmers/Level-2/짝지어 제거하기/solution.js
@@ -0,0 +1,13 @@
+function solution(s) {
+  const stack = [];
+
+  for (const word of s) {
+    if (stack[stack.length - 1] === word) {
+      stack.pop();
+    } else {
+      stack.push(word);
+    }
+  }
+
+  return stack.length === 0 ? 1 : 0;
+}
